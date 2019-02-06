a4f019dc331e665bed94ffa03debaf6306ef3214
Neel M <neel_mehta@asl.org>
2019-02-06 16:43:30 +0000

    Added cargo intake and arm subsystem and commands
    Added intake arm subsystem
    Added 3 commands:
    ArmUp
    ArmDown
    StartWheels
    + 1 command group to run arm up and start wheels, called begin intake
    Robot: public static final IntakeArm intakeArm = new IntakeArm();
    OI:
    public class OI {
    
      public final Joystick stick = new Joystick(0);
      public Joystick operator = new Joystick(1);
    
      public JoystickButton deployArmButton = new JoystickButton(operator, 1);
      public JoystickButton retractArmButton = new JoystickButton(operator, 2);
      public OI() {
        deployArmButton.whileHeld(new BeginIntake());
        retractArmButton.whenPressed(new ArmDown());
      }

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.ArmDown;
+import frc.robot.commands.BeginIntake;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -16,5 +19,13 @@ import edu.wpi.first.wpilibj.Joystick;
 public class OI {
 
   public final Joystick stick = new Joystick(0);
+  public Joystick operator = new Joystick(1);
+
+  public JoystickButton deployArmButton = new JoystickButton(operator, 1);
+  public JoystickButton retractArmButton = new JoystickButton(operator, 2);
+  public OI() {
+    deployArmButton.whileHeld(new BeginIntake());
+    retractArmButton.whenPressed(new ArmDown());
+  }
 
 }
